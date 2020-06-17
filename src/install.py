77e4c62cce24475cf5f7a874743d40249e9f4832
Jacob Cannizzaro <jacobcannizzaro@gwu.edu>
2020-06-17 19:40:58 -0400

    comment code

--- src/install.py
+++ src/install.py
@@ -1,19 +1,20 @@
 import pickle
 
-
+#initialize dictionary
 credentials = {
     "thingname": "",
     "endpoint": "",
     "pathname": ""
 }
 
+#fill dictionary with user input
 credentials['thingname'] = input("Enter your thing-name (as registered on AWS): ")
 
 credentials['endpoint'] = input("Enter the AWS Broker endpoint: ")
 
 credentials['pathname'] = input("Enter the pathname to <.../>connect_device_package/: ")
 
-
+#save this dictionary's state by pickling it
 pickle.dump(credentials, open("connection.pkl", "wb"))
 
 print("Done installing, run this script anytime to update connection credentials!")
--- src/sempi.py
+++ src/sempi.py
@@ -8,7 +8,28 @@ import pickle
 from tkinter import *
 from tkinter import messagebox
 from Crypto.Cipher import AES
+#Above import is from PyCrypto library
+#AES (Advanced Encryption Standard) is a symmetric block cipher standardized by NIST
+"""
+Below is an example call using this library that needs the most explanation:
+obj = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR')
 
+*   This creates a new cipher object in obj.
+*   The first argument is they key (gotten from the user) that can be 16, 24, 
+    or 32 bytes (chars) long. This is the key to be used for the symmetric cipher.
+    The second argument s
+*   The second argument specifies the mode
+*   Third argument specifies the initialization vector for encryption/decryption
+    This must be 16 bytes long and the same for the decryption cipher object as
+    the encryption cipher object. Therefore I randomly generated a 16 char string
+    to serve as the iv and hardcoded it instead of computing it randomly as is
+    often done.
+
+read more: https://www.dlitz.net/software/pycrypto/api/current/
+"""
+
+
+#initialize global variables
 connflag = False
 clientRunning = False
 pubtop = ""
@@ -76,20 +97,21 @@ def handleProblem(window):
 def handleErrorMessage(window1, window2): 
 	window1.deiconify()
 	window2.destroy()
-	
+
+#button handler for send button
 def enterPressed():
 	 global client
 	 global e
-	 sent = e.get()
+	 sent = e.get() #grab text from text input field
 	 strippedString = sent.strip()
 	 if not strippedString:
 	 	  input_user.set('')
 	 	  return
 	 input_user.set('')
 	 if connflag == True:
-	 	  obj = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR')
-	 	  ciphertext = obj.encrypt(sent)
-	 	  client.publish(pubtop, ciphertext)
+	 	  obj = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR') #see under imports for explanation
+	 	  ciphertext = obj.encrypt(sent) #encrypt msg -> ciphertext
+	 	  client.publish(pubtop, ciphertext) #publish encrypted msg
 	 	  messages.configure(state="normal")
 	 	  messages.insert(INSERT, 'Me: %s\n\n' % sent)
 	 	  messages.configure(state="disabled")  
@@ -106,7 +128,7 @@ def Enter_pressed(event):
     	  return
     input_user.set('')
     if connflag == True:
-        obj = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR')
+        obj = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR') #see under imports for explanation
         ciphertext = obj.encrypt(sent)
         client.publish(pubtop, ciphertext) 
         messages.configure(state="normal")
@@ -114,28 +136,32 @@ def Enter_pressed(event):
         messages.configure(state="disabled")   
         messages.see("end") 
 
+#handler method waiting for connection to broker
 def on_connect(client, userdata, flags, rc):
     global connflag
     global msg
     messages.configure(state="normal")
     messages.insert(INSERT, "Connected to AWS\n\n")
     messages.configure(state="disabled")
-    connflag = True
+    connflag = True #update global connection flag
 
+#handler method executes when client recieves a message from broker
 def on_message(client, userdata, message):
     global puptop
     global msg
     if str(message.topic) != pubtop:
-        obj2 = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR')
-        recvmsgEncrypted = message.payload
-        m = obj2.decrypt(recvmsgEncrypted)
-        gen_topic, sender = message.topic.split('/')
-        sender_message = sender + ": " + m.decode('utf-8')
+        obj2 = AES.new(str(key), AES.MODE_CFB, 's7a6sTM58ZBLiNpR') #see under imports for explanation
+        recvmsgEncrypted = message.payload  #recieve message from broker
+        m = obj2.decrypt(recvmsgEncrypted)  #decrypt ciphertext -> m
+        gen_topic, sender = message.topic.split('/')       #grab the 'jakepi' from 'chat/jakepi' topic
+        sender_message = sender + ": " + m.decode('utf-8') #decrypted text is in bytes format, have to decode
         messages.configure(state="normal")
         messages.insert(INSERT, '%s\n\n' % sender_message)
         messages.configure(state="disabled")
         messages.see("end")
         
+
+#following 3 methods just used for debugging purposes --#
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed:", str(mid), str(grant))
 
@@ -145,45 +171,55 @@ def on_unsubscribe(client, userdata, mid):
 def on_disconnect(client, userdata, rc):
     if rc != 0:
         print("unexpected Disconnection")
+#-------------------------------------------------------#
 
 def connect_client():
     global pubtop
     global subtop
     global client
+    #point client object's functions to the functions implemented above
     client.on_connect = on_connect
     client.on_message = on_message
     client.on_subscribe = on_subscribe
     client.on_unsubscribe = on_unsubscribe
     client.on_disconnect = on_disconnect
+    #load pickled credential dictionary to grab AWS required connection credentials
     credentials = pickle.load(open("connection.pkl", "rb"))
     awshost = credentials['endpoint']
-    awsport = 8883                                              # Port no.
-    clientId = credentials['thingname']                                   # Thing_Name
-    thingName = credentials['thingname']                                    # Thing_Name
-    caPath = credentials['pathname'] + "connect_device_package/root-CA.crt"                                      # Root_CA_Certificate_Name
-    certPath = credentials['pathname'] + "connect_device_package/" + thingName + ".cert.pem"                            # <Thing_Name>.cert.pem
-    keyPath = credentials['pathname'] + "connect_device_package/" + thingName + ".private.key"
-    client.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)  # pass parameters
+    awsport = 8883                          # Port no.
+    clientId = credentials['thingname']     # Thing_Name
+    thingName = credentials['thingname']    # Thing_Name
+    caPath = credentials['pathname'] + "connect_device_package/root-CA.crt"                           # Root_CA_Certificate_Name
+    certPath = credentials['pathname'] + "connect_device_package/" + thingName + ".cert.pem"          # <Thing_Name>.cert.pem
+    keyPath = credentials['pathname'] + "connect_device_package/" + thingName + ".private.key"        # path to keys
+    #set aws credentials for client
+    client.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)  
     client.connect(awshost, awsport, keepalive=60)               # connect to aws server
-    sleep(1) #ensure correct order... maybe not needed
-    pubtop = "chat/" + thingName
-    subtop = "chat/#"
+    sleep(1) #ensure correct order... 
+
+    pubtop = "chat/" + thingName #topic client publishes messages to
+    subtop = "chat/#"            #topic client subscribes to (# is wildcard that allows subscribing to all chat/subtopics)
     client.subscribe(subtop)
     sleep(1)
     clientRunning = True
-    client.loop_start()
+    client.loop_start()          #this thread is now just looping, waiting for subscribed topics being relayed from broker
 
+
+#function is run after session key is entered to use this thread to connect and listen to the AWS broker
 def subThread():
-	 iThread = threading.Thread(target = connect_client)
+	 iThread = threading.Thread(target = connect_client) #target method is connect_client() - this is where iThread begins executing
 	 iThread.start()
 
+#window closing event handler
 def on_closing():
     global clientRunning
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        if clientRunning == True:
+        if clientRunning == True: #if client is running stop the loop
             clientRunning = False
             client.loop_stop()
-        win.destroy()
+        win.destroy()             #quit program
+
+
 
 win = Tk() #establish window
 btn2 = Button(win, text="Connect", command=popup) #define connect button
